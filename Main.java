ccf5393bed1129d152255cbec53395d28724154b
mina-baba <ax040404@gmail.com>
2026-03-26 04:35:19 +0200

    discounts

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ class main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("would you like some? ");
         String answ = scanner.next();
+        double cost=0;
         if (answ.equals("yes")) {
             System.out.println("i have many options" + " and sizes");
             System.out.println("small \n" + "medium \n" + "large \n");
@@ -28,33 +29,61 @@ class main {
             String opt = scanner.next();
             System.out.println("how many would you like?");
             Integer nb = scanner.nextInt();
+
             if (opt.equals(pizzax)) {
                 if (size.equals(small)) {
-                    System.out.println("your cost would be " + 10.99 * nb);
+                    cost =10.99* nb;
+                    System.out.println("your cost would be " + cost);
                 } else if (size.equals(medium)) {
-                    System.out.println("your cost would be " + 11.99 * nb);
+                    cost =11.99* nb;
+                    System.out.println("your cost would be " + cost);
                 } else if (size.equals(large)) {
-                    System.out.println("your cost would be " + 12.99 * nb);
+                    cost =12.99* nb;
+                    System.out.println("your cost would be " + cost);
                 }
             } else if (opt.equals(pizzay)) {
                 if (size.equals(small)) {
-                    System.out.println("your cost would be " + 11.99 * nb);
+                    cost =11.99* nb;
+                    System.out.println("your cost would be " + cost);
                 } else if (size.equals(medium)) {
-                    System.out.println("your cost would be " + 12.99 * nb);
+                    cost =12.99* nb;
+                    System.out.println("your cost would be " + cost);
                 } else if (size.equals(large)) {
-                    System.out.println("your cost would be " + 13.99 * nb);
+                    cost =13.99* nb;
+                    System.out.println("your cost would be " + cost);
                 }
             } else if (opt.equals(pizzaz)) {
                 if (size.equals(small)) {
-                    System.out.println("your cost would be " + 12.99 * nb);
+                    cost =12.99* nb;
+                    System.out.println("your cost would be " + cost);
                 } else if (size.equals(medium)) {
-                    System.out.println("your cost would be " + 13.99 * nb);
+                    cost =13.99* nb;
+                    System.out.println("your cost would be " + cost);
                 } else if (size.equals(large)) {
-                    System.out.println("your cost would be " + 14.99 * nb);
+                    cost =14.99* nb;
+                    System.out.println("your cost would be " + cost);}
                 }
-            } else {
-                System.out.print("ok");
-            }
-        }
+            System.out.print("Are you a student?(true/false)\n");
+            boolean student = scanner.nextBoolean();
+            System.out.print("Are you above 65?(true/false)\n");
+            boolean senior = scanner.nextBoolean();
+            double modPrice;
+            modPrice = 0;
+            double modPrice1;
+            modPrice1 = 0;
+            double finalPrice;
+            finalPrice = 0;
+            if(true == student ){
+                modPrice = (cost*0.15);}
+            if(true == senior) {
+                modPrice1 = ( cost * 0.15);}
+            if (student== false&&senior == false){
+                System.out.print("no discount for you\n");}
+            finalPrice=cost - (modPrice+modPrice1);
+                System.out.printf("your finalized cost would be %.2f",finalPrice);}
+
+
+        else{System.out.print("ok");}
+
     }
 }
